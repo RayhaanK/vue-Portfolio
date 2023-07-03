6db6cea49d1604bc06545785bd0e4535e8a9cd3d
RayhaanK <rayhaankhan331@gmail.com>
2023-07-03 09:00:34 +0200

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,22 @@
 <template>
-  <div class="container">
-    <footer>
-      <div class="icons">
-        <a href="https://www.linkedin.com/in/rayhaan-khan-9ba792263/"
-          ><i class="fa-brands fa-linkedin fa-2xl" style="color: #84a7a1"></i
-        ></a>
-        <a href="https://github.com/RayhaanK"
-          ><i
-            class="fa-brands fa-square-github fa-2xl"
-            style="color: #84a7a1"
-          ></i
-        ></a>
-      </div>
-      <div class="text">
-        <p>
-          Gmail: rayhaankhan04@gmail.com <br />
-          Copyright &copy; 2023
-        </p>
-      </div>
-    </footer>
-  </div>
+  <footer>
+    <div class="icons">
+      <a
+        href="https://www.linkedin.com/in/rayhaan-khan-9ba792263/"
+        target="_blank"
+        ><i class="fa-brands fa-linkedin fa-2xl" style="color: #84a7a1"></i
+      ></a>
+      <a href="https://github.com/RayhaanK" target="_blank"
+        ><i class="fa-brands fa-square-github fa-2xl" style="color: #84a7a1"></i
+      ></a>
+    </div>
+    <div class="text">
+      <p>
+        Gmail: rayhaankhan04@gmail.com <br />
+        Copyright &copy; 2023
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -29,15 +26,17 @@ export default {};
 <style scoped>
 i {
   padding: 0.7rem;
+  margin-top: 1rem;
+  margin-bottom: 0.4rem;
 }
 
-.container {
-    width: 100%;
-    height: 20vh;
-    background-color: #0E2954;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+footer {
+  width: 100%;
+  height: 15vh;
+  background-color: #0e2954;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
